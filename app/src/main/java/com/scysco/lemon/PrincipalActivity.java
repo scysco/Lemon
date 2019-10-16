928c54c8d1f2e4d326b4e3e2e7008b00974fb998
Jesus Sanchez <scysco@gmail.com>
2019-10-15 19:27:29 -0500

    second commit

--- app/src/main/java/com/scysco/lemon/PrincipalActivity.java
+++ app/src/main/java/com/scysco/lemon/PrincipalActivity.java
@@ -3,10 +3,15 @@ package com.scysco.lemon;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.flexbox.FlexboxLayout;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -17,7 +22,17 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
         getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimary));
+
+        addButton();
+    }
+    private void addButton() {
+        FlexboxLayout fbContainer = findViewById(R.id.fbContainer);
+        ImageView btnUno = new ImageView(this);
+        btnUno.setImageResource(R.mipmap.ic_launcher);
+        btnUno.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        fbContainer.addView(btnUno);
     }
 }

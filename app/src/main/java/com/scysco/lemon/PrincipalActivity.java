89efa99534ce92bfc94e6afd6f8a47bf4b209983
Jesus Sanchez <scysco@gmail.com>
2019-10-17 16:34:16 -0500

    comment

--- app/src/main/java/com/scysco/lemon/PrincipalActivity.java
+++ app/src/main/java/com/scysco/lemon/PrincipalActivity.java
@@ -24,17 +24,6 @@ public class PrincipalActivity extends AppCompatActivity {
     private static final String TAG = "PrincipalActivity";
     public static String PLACE;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
-
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimary));
-
-        PrincipalActivity.PLACE = "Products";
-
         /*
         Product p = new Product("Duquesa",500,10,20);
         FirebaseFirestore DB = FirebaseFirestore.getInstance();
@@ -52,6 +41,18 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });*/
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimary));
+
+        PrincipalActivity.PLACE = "Products";
+
     }
     private void addButton() {
         FlexboxLayout fbContainer = findViewById(R.id.fbContainer);
@@ -62,4 +63,6 @@ public class PrincipalActivity extends AppCompatActivity {
                 ViewGroup.LayoutParams.WRAP_CONTENT));
         fbContainer.addView(btnUno);
     }
+
+
 }
